a8e39f8def3d39afe5e2b1c4acfa5da81602f187
Mas4erLomas4er <i9997117@gmail.com>
2018-08-25 19:59:01 +0300

    'add'

--- public/Js/app.js
+++ public/Js/app.js
@@ -1,45 +1,42 @@
-// siteMenu();
-//
-//
-//
-// function siteMenu() {
-//
-//     let submenus = document.querySelectorAll('nav.main .submenu >a');
-//     submenus.forEach(function (item) {
-//         item.addEventListener('click', function (e) {
-//             this.parentNode.classList.toggle('opened');
-//             this.querySelectorAll('.submenu')
-//                 .forEach((sub) => sub.parentNode.classList.remove('opened'));
-//             e.stopPropagation();
-//         })
-//     });
-// }
-//
-// let burger = document.querySelector('#burger-button');
-// let body = document.querySelector('body');
-// let nav = document.querySelector('nav.main');
-// let timeout;
-//
-// nav.addEventListener('mouseover', function () {
-//     clearTimeout(timeout);
-// });
-//
-// nav.addEventListener('mouseout', function () {
-//     clearTimeout(timeout);
-//     timeout = setTimeout(() => {
-//         body.classList.remove('menu-open');
-// }, 2000);
-// });
+siteMenu();
+function siteMenu() {
 
+    let submenus = document.querySelectorAll('nav.main .submenu >a');
+    submenus.forEach(function (item) {
+        item.addEventListener('click', function (e) {
+            this.parentNode.classList.toggle('opened');
+            this.querySelectorAll('.submenu')
+                .forEach((sub) => sub.parentNode.classList.remove('opened'));
+            e.stopPropagation();
+        })
+    });
+}
+
+let burger = document.querySelector('#burger-button');
+let body = document.querySelector('body');
+let nav = document.querySelector('nav.main');
+let timeout;
+
+nav.addEventListener('mouseover', function () {
+    clearTimeout(timeout);
+});
+
+nav.addEventListener('mouseout', function () {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+        body.classList.remove('menu-open');
+}, 2000);
+});
+burger.addEventListener('click', function () {
+     body.classList.toggle('menu-open');
+});
 
 // // работает по нажатию на бургер
 //
 // let burger = document.querySelector('#burger-button');
 // let body = document.querySelector('body');
 //
-// burger.addEventListener('click', function () {
-//     body.classList.toggle('menu-open');
-// });
+//
 //
 //
 // // функция которая покажет меню при наведении мыши
@@ -67,43 +64,44 @@
 //
 // //после двух секунд меню прячется
 //
-let burger = document.querySelector('#burger-button');
-let body = document.querySelector('body');
-let nav = document.querySelector('nav.main');
-let timeout;
-
-nav.addEventListener('mouseover', function () {
-    clearTimeout(timeout);
-});
-
-nav.addEventListener('mouseout', function () {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => {
-        body.classList.remove('menu-open');
-    }, 2000);
-});
-
-burger.addEventListener('click', function () {
-    body.classList.toggle('menu-open');
-});
-
-
-
-
-siteMenu(); //функция для открвывания вложенных ul
-
-function siteMenu() {
-    //меню сйта
-    let submenus = document.querySelectorAll('nav.main .submenu >a');
-    submenus.forEach(function (item) {
-        item.addEventListener('click', function (e) {
-            this.parentNode.classList.toggle('opened');
-            this.querySelectorAll('.submenu')
-                .forEach((sub) => sub.parentNode.classList.remove('opened'));
-            e.stopPropagation();
-        })
-    });
-}
+// let burger = document.querySelector('#burger-button');
+// let body = document.querySelector('body');
+// let nav = document.querySelector('nav.main');
+// let timeout;
+//
+// nav.addEventListener('mouseover', function () {
+//     clearTimeout(timeout);
+// });
+//
+// nav.addEventListener('mouseout', function () {
+//     clearTimeout(timeout);
+//     timeout = setTimeout(() => {
+//         body.classList.remove('menu-open');
+//     }, 2000);
+// });
+//
+// burger.addEventListener('click', function () {
+//     body.classList.toggle('menu-open');
+//     burger.classList.toggle('close-button');
+// });
+//
+//
+//
+//
+// siteMenu(); //функция для открвывания вложенных ul
+//
+// function siteMenu() {
+//     //меню сйта
+//     let submenus = document.querySelectorAll('nav.main .submenu >a');
+//     submenus.forEach(function (item) {
+//         item.addEventListener('click', function (e) {
+//             this.parentNode.classList.toggle('opened');
+//             this.querySelectorAll('.submenu')
+//                 .forEach((sub) => sub.parentNode.classList.remove('opened'));
+//             e.stopPropagation();
+//         })
+//     });
+// }
 
 
 
