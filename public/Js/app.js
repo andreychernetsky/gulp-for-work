7005767b09b406fa9aef2ead4c6dbafdb9cdb658
Mas4erLomas4er <i9997117@gmail.com>
2018-08-27 19:29:56 +0300

    h/

--- public/Js/app.js
+++ public/Js/app.js
@@ -1,7 +1,7 @@
 // siteMenu();
 // function siteMenu() {
 //
-//     let submenus = document.querySelectorAll('nav.main .submenu >a');
+//     let submenus = document.querySelectorAll('nav.app.scss .submenu >a');
 //     submenus.forEach(function (item) {
 //         item.addEventListener('click', function (e) {
 //             this.parentNode.classList.toggle('opened');
@@ -11,58 +11,6 @@
 //         })
 //     });
 // }
-//
-// let burger = document.querySelector('#burger-button');
-// let body = document.querySelector('body');
-// let nav = document.querySelector('nav.main');
-// let timeout;
-//
-// nav.addEventListener('mouseover', function () {
-//     clearTimeout(timeout);
-// });
-//
-// nav.addEventListener('mouseout', function () {
-//     clearTimeout(timeout);
-//     timeout = setTimeout(() => {
-//         body.classList.remove('menu-open');
-// }, 2000);
-// });
-// burger.addEventListener('click', function () {
-//      body.classList.toggle('menu-open');
-// });
-
-// // работает по нажатию на бургер
-//
-// let burger = document.querySelector('#burger-button');
-// let body = document.querySelector('body');
-//
-//
-//
-//
-// // функция которая покажет меню при наведении мыши
-//
-// window.onload = function() {
-//
-//     let menuShow =()=> {
-//         document.querySelector('.main').style.marginLeft = 0;
-//     }
-//     let menuHide = ()=> {
-//         document.querySelector('.main').style.marginLeft = '-320px';
-//     }
-//     document.querySelector('.main').onmouseover = menuShow;
-//     document.querySelector('.main').onmouseout = menuHide;
-// }
-//
-//
-// let burger = document.querySelector('#burger-button');
-// let body = document.querySelector('body');
-//
-// burger.addEventListener('click', function () {
-//     body.classList.toggle('menu-open');
-// });
-//
-//
-// //после двух секунд меню прячется
 //
 let burger = document.querySelector('#burger-button');
 let body = document.querySelector('body');
@@ -77,13 +25,66 @@ nav.addEventListener('mouseout', function () {
     clearTimeout(timeout);
     timeout = setTimeout(() => {
         body.classList.remove('menu-open');
-    }, 2000);
+}, 2000);
+});
+burger.addEventListener('click', function () {
+     body.classList.toggle('menu-open');
+     console.log('1');
 });
 
-burger.addEventListener('click', function () {
-    body.classList.toggle('menu-open');
-    burger.classList.toggle('close-button');
-});
+// // работает по нажатию на бургер
+//
+// let burger = document.querySelector('#burger-button');
+// let body = document.querySelector('body');
+//
+//
+//
+//
+// // функция которая покажет меню при наведении мыши
+//
+// window.onload = function() {
+//
+//     let menuShow =()=> {
+//         document.querySelector('.app.scss').style.marginLeft = 0;
+//     }
+//     let menuHide = ()=> {
+//         document.querySelector('.app.scss').style.marginLeft = '-320px';
+//     }
+//     document.querySelector('.app.scss').onmouseover = menuShow;
+//     document.querySelector('.app.scss').onmouseout = menuHide;
+// }
+//
+//
+// let burger = document.querySelector('#burger-button');
+// let body = document.querySelector('body');
+//
+// burger.addEventListener('click', function () {
+//     body.classList.toggle('menu-open');
+// });
+//
+//
+// //после двух секунд меню прячется
+//
+// let burger = document.querySelector('#burger-button');
+// let body = document.querySelector('body');
+// let nav = document.querySelector('nav.app.scss');
+// let timeout;
+//
+// nav.addEventListener('mouseover', function () {
+//     clearTimeout(timeout);
+// });
+//
+// nav.addEventListener('mouseout', function () {
+//     clearTimeout(timeout);
+//     timeout = setTimeout(() => {
+//         body.classList.remove('menu-open');
+//     }, 2000);
+// });
+//
+// burger.addEventListener('click', function () {
+//     body.classList.toggle('menu-open');
+//     burger.classList.toggle('close-button');
+// });
 //
 //
 //
@@ -92,7 +93,7 @@ burger.addEventListener('click', function () {
 //
 // function siteMenu() {
 //     //меню сйта
-//     let submenus = document.querySelectorAll('nav.main .submenu >a');
+//     let submenus = document.querySelectorAll('nav.app.scss .submenu >a');
 //     submenus.forEach(function (item) {
 //         item.addEventListener('click', function (e) {
 //             this.parentNode.classList.toggle('opened');
@@ -113,7 +114,7 @@ window.onscroll = function () {
         fixedSidebar.style.top = 20 + 'px';
         fixedSidebar.style.right = 20 + 'px';
         fixedSidebar.style.height = document.documentElement.clientHeight - 40 + 'px';
-        mainContent.style.marginRight = fixedSidebar.clientWidth + 40 +'px';
+        mainContent.style.marginRight = fixedSidebar.clientWidth + 37 +'px';
     } else {
         fixedSidebar.style.position = 'static';
         fixedSidebar.style.height = 900 + 'px';
